c0e3f60b337904b7c296d57bf1f78ffc15da5953
j-nowland <127156607+j-nowland@users.noreply.github.com>
2023-04-19 14:19:00 +1200

    outline

---
+++ src/game.js
@@ -0,0 +1,20 @@
+// put functions (as variables) at the top
+
+// define the game board size and mine count
+
+// define the game board and mine locations
+
+// create the game board
+
+// create a nested array with the specified size
+
+//place the mines randomly on the board
+
+// calculate the number of neighbouring mines for each cell
+
+// reveal a cell on the board
+
+//flag a cell on the board
+
+// reveal all mines on the board
+
